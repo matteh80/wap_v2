df66470c4c5b2f009ad3acea5d81e7d12bce93ec
Mathias Hedström <hedstrom.mathias@gmail.com>
2017-08-21 13:43:50 +0200

    Truckkort sub select

--- src/routes/DrivingLicenses/LicenseForm/LicenseForm.js
+++ src/routes/DrivingLicenses/LicenseForm/LicenseForm.js
@@ -24,14 +24,18 @@ class LicenseForm extends React.Component {
 
     this.state = {
       selectValue: null,
-      collapse: !this.props.notEmpty
+      truckSelectValue: null,
+      collapse: !this.props.notEmpty,
+      truckCollapse: false
     }
 
-    this.props.drivinglicenses.length === 0 && dispatch(getAllLicenses())
+    this.props.drivinglicenses.licenses.length === 0 && dispatch(getAllLicenses())
+
     dispatch(getMyLicenses())
 
     this.toggleCollapse = this.toggleCollapse.bind(this)
     this._getOptions = this._getOptions.bind(this)
+    this._getTruckOptions = this._getTruckOptions.bind(this)
     this._handleLicenseChange = this._handleLicenseChange.bind(this)
   }
 
@@ -43,7 +47,34 @@ class LicenseForm extends React.Component {
 
     $.each(licenses, function (i, item) {
       index = userLicenses.findIndex(userLicenses => userLicenses.id === item.id)
-      if (index === -1) {
+      if (index === -1 && !item.name.includes('Truckkort ')) {
+        optiondata.push({
+          label: item.name,
+          value: item.id,
+          name: item.name,
+          id: item.id,
+        })
+      }
+    })
+
+    optiondata.sort(function (a, b) {
+      if (a.label < b.label) return -1
+      if (a.label > b.label) return 1
+      return 0
+    })
+
+    return optiondata
+  }
+
+  _getTruckOptions () {
+    console.log('GET OPTIONS')
+    let { licenses, userLicenses } = this.props.drivinglicenses
+    let optiondata = []
+    let index = -1
+
+    $.each(licenses, function (i, item) {
+      index = userLicenses.findIndex(userLicenses => userLicenses.id === item.id)
+      if (index === -1 && item.name.includes('Truckkort ')) {
         optiondata.push({
           label: item.name,
           value: item.id,
@@ -63,7 +94,19 @@ class LicenseForm extends React.Component {
   }
 
   _handleLicenseChange (value) {
-    this.props.onAdd(value)
+    console.log(value)
+    if (value.label === 'Truckkort') {
+      this.setState({
+        selectValue: value,
+        truckCollapse: true
+      })
+    } else {
+      this.props.onAdd(value)
+      this.setState({
+        selectValue: null,
+        truckCollapse: false
+      })
+    }
   }
 
   toggleCollapse () {
@@ -99,11 +142,20 @@ class LicenseForm extends React.Component {
                 <FormGroup>
                   <Select
                     options={this._getOptions()}
-                    clearable
+                    clearable={false}
                     onChange={this._handleLicenseChange}
                     placeholder='Välj körkort'
                     value={this.state.selectValue}
                   />
+                  <Collapse isOpen={this.state.truckCollapse}>
+                    <Select
+                      options={this._getTruckOptions()}
+                      clearable
+                      onChange={this._handleLicenseChange}
+                      placeholder='Välj typ av truckkort'
+                      value={this.state.truckSelectValue}
+                    />
+                  </Collapse>
                 </FormGroup>
               </Form>
             </Collapse>
