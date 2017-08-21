055730158fa5ae0b8eb254751d85848b8715decb
Mathias Hedström <hedstrom.mathias@gmail.com>
2017-08-21 11:35:52 +0200

    404 update

--- src/routes/NotFound404/NotFound404.js
+++ src/routes/NotFound404/NotFound404.js
@@ -10,8 +10,11 @@ class NotFound404 extends React.Component {
   render () {
     return (
       <Container>
-        <Row className='justify-content-center align-items-center' style={{ minHeight: '100vh' }}>
-          <Col>
+        <Row className='justify-content-center align-items-center flex-column' style={{ minHeight: '100vh' }}>
+          <Col xs='12' md='8' lg='6'>
+            <img src='/img/404.png' className='img-fluid' />
+          </Col>
+          <Col xs='12'>
             <h1 className='text-center'>Oops! Sidan du söker finns inte</h1>
           </Col>
         </Row>
